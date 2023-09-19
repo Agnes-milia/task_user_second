f9af37a378fdea2b4d4898f3b6f45a0f3d7a3849
Agnes-milia <ratipotti@gmail.com>
2023-09-19 15:51:28 +0200

    UserController list működik

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(){
+        $tasks = response()->json(Task::all());
+        return $tasks;
+    }
+
+    public function show($id){
+        //az azonosító alapján megkeresi 
+        $task = response()->json(Task::find($id));
+        return $task;
+    }
+
+    public function destroy($id){
+        //az azonosító alapján megkeresi 
+        Task::find($id)->delete;
+        return redirect('/task/list');
+    }
+
+    public function store(Request $request){
+        $task = new Task();
+        $task->title = $request->title;
+        $task->description = $request->description;
+        $task->end_date = $request->end_date;
+        $task->user_id = $request->user_id;
+        $task->status = $request->status;
+        $task->save();
+        return redirect('/task/list');
+    }
+
+    public function update(Request $request, $id){
+        $task = Task::find($id);
+        $task->title = $request->title;
+        $task->description = $request->description;
+        $task->end_date = $request->end_date;
+        $task->user_id = $request->user_id;
+        $task->status = $request->status;
+        $task->save();
+        return redirect('/task/list');
+    }
+
+    //view file-ok
+    public function listView(){
+        $tasks = Task::all();
+        return view('task.list', ['tasks' => $tasks]);
+    }
+
+    public function newView()
+    {
+        //új rekord rögzítése
+        $users = User::all();
+        return view('task.new', ['users' => $users]);
+    }
+
+    public function editView($id)
+    {
+        $users = User::all();
+        $task = Task::find($id);
+        return view('task.edit', ['users' => $users, 'task' => $task]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,25 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
-
+//task útvonalak
+Route::get('/api/tasks', [TaskController::class, 'index']);
+Route::get('/api/tasks/{id}', [TaskController::class, 'show']);
+/* GET  -1  /api/task/1 */
+Route::get('/api/tasks/{id}', [TaskController::class,'show']); 
+/* POST     /api/task */
+Route::post('/api/tasks', [TaskController::class,'store']);
+/* PUT      /api/task/1 */
+Route::put('/api/tasks/{id}', [TaskController::class,'update']);
+/* DELETE       /api/task/1 */
+Route::delete('/api/tasks/{id}', [TaskController::class,'destroy']);
+//user útvonalak
+Route::get('/api/users', [UserController::class, 'index']);
+Route::get('/api/users/{id}', [UserController::class, 'show']);
+//view útvonalak
+Route::get('/task/list', [TaskController::class, 'listView']);
+Route::get('/task/new', [TaskController::class, 'newView']);
+/* Task módosítása      /task/edit/1 */
+Route::get('/task/edit/{id}', [TaskController::class, 'editView']);
+//user
+Route::get('/user/list', [UserController::class, 'listView']);
 //require __DIR__ . '/auth.php';
